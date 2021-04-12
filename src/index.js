ef751b020effc6ea54725bd077c0cbd90e331dc8
root <root@ip-172-31-44-167.ap-south-1.compute.internal>
2021-04-12 13:53:45 +0000

    aws chime s3 object url

---
+++ src/index.js
@@ -0,0 +1,171 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+'use strict';
+
+var AWS = require('aws-sdk');
+var ecs = new AWS.ECS();
+
+// Reading environment variables
+const ecsClusterArn = process.env.ecsClusterArn;
+const ecsTaskDefinationArn = process.env.ecsTaskDefinationArn;
+const ecsContainerName = process.env.ecsContainerName;
+const recordingArtifactsBucket = process.env.recordingArtifactsBucket;
+
+let responseBody = {
+    message: '',
+    input: ''
+};
+
+let response = {
+    statusCode: 200,
+    headers: {},
+    body: ''
+};
+ 
+exports.handler = function(event, context, callback) {
+    let meetingURL = "";
+    let taskId = "";
+    let recordingAction = "";
+    
+    console.log(event);
+    responseBody.input = event;
+    
+    if(event.queryStringParameters && event.queryStringParameters.recordingAction) {
+        console.log("Recording action: " + event.queryStringParameters.recordingAction);
+        recordingAction = event.queryStringParameters.recordingAction;
+    }
+    
+    switch(recordingAction.toLowerCase()) {
+        case 'start':
+            if(event.queryStringParameters && event.queryStringParameters.meetingURL) {
+                console.log("Meeting URL: " + event.queryStringParameters.meetingURL);
+                meetingURL = decodeURIComponent(event.queryStringParameters.meetingURL);
+                return startRecording(event, context, callback, meetingURL);
+            } else {
+                responseBody = {
+                    message: "Missing parameter: meetingURL",
+                    input: event
+                };
+                response = {
+                    statusCode: 400,
+                    headers: {},
+                    body: JSON.stringify(responseBody, null, ' ')
+                };
+                context.succeed(response);
+            }
+        case 'stop':
+            if(event.queryStringParameters && event.queryStringParameters.taskId) {
+                console.log("ECS task ID: " + event.queryStringParameters.taskId);
+                taskId = event.queryStringParameters.taskId;
+                return stopRecording(event, context, taskId);
+            } else {
+                responseBody = {
+                    message: "Missing parameter: taskId",
+                    input: event
+                };
+                response = {
+                    statusCode: 400,
+                    headers: {},
+                    body: JSON.stringify(responseBody, null, ' ')
+                };
+                context.succeed(response);
+            }
+        default:
+            responseBody = {
+                message: "Invalid parameter: recordingAction. Valid values 'start' & 'stop'",
+                input: event
+            };
+            response = {
+                statusCode: 400,
+                headers: {},
+                body: JSON.stringify(responseBody)
+            };
+    }
+    
+    console.log("response: " + JSON.stringify(response));
+    callback(null, response);
+};
+
+function getFileKey() {
+    const timestamp = new Date();
+    const fileTimestamp = timestamp.toISOString().substring(0,19);
+    const year = timestamp.getFullYear();
+    const month = timestamp.getMonth() + 1;
+    const day = timestamp.getDate();
+    const hour = timestamp.getUTCHours();
+    return `${year}/${month}/${day}/${hour}/${fileTimestamp}.mp4`;
+}
+
+function startRecording(event, context, callback, meetingUrl) {
+    const fileKey = getFileKey();
+    let ecsRunTaskParams = {
+        cluster: ecsClusterArn,
+        launchType: "EC2",
+        count: 1,
+        overrides: {
+            containerOverrides: [ 
+                 { 
+                    environment: [ 
+                        { 
+                            name: "MEETING_URL",
+                            value: meetingUrl
+                        },
+                        {
+                            name: "RECORDING_ARTIFACTS_BUCKET",
+                            value: recordingArtifactsBucket
+                        },
+			{
+                            name: "FILE_KEY",
+                            value: fileKey
+                        }
+                    ],
+                    name: ecsContainerName
+                }
+            ],
+        },
+        placementConstraints: [{
+            type: "distinctInstance"
+        }],
+        taskDefinition: ecsTaskDefinationArn
+    };
+    
+    ecs.runTask(ecsRunTaskParams, function(err, data) {
+        if (err) {
+            console.log(err);   // an error occurred
+            response.statusCode = err.statusCode;
+            response.body = JSON.stringify(err, null, ' ');
+            context.succeed(response);
+        }
+        else {
+            console.log(data);  // successful response
+            response.statusCode = 200;
+            response.body = JSON.stringify({taskId: (data.tasks.length && data.tasks[0].taskArn) ? data.tasks[0].taskArn : data, fileKey}, null, ' ');
+            context.succeed(response);
+        }
+    });
+}
+
+function stopRecording(event, context, taskId) {
+    let ecsStopTaskParam = {
+        cluster: ecsClusterArn,
+        task: taskId
+    };
+    
+    ecs.stopTask(ecsStopTaskParam, function(err, data) {
+        if (err) {
+            console.log(err);   // an error occurred
+            response.statusCode = err.statusCode;
+            response.body = JSON.stringify(err, null, ' ');
+            context.succeed(response);
+        }
+        else {
+            console.log(data);  // successful response
+            response.statusCode = 200;
+            responseBody = data;
+            response.body = JSON.stringify(data, null, ' ');
+            console.log("Stop task succeeded.", response);
+            context.succeed(response);
+        }
+    });
+}
